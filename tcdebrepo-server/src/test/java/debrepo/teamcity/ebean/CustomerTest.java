a67e73298ad5cc05e740e61b882f4823e239e465
Net Wolf UK <netwolfuk@gmail.com>
2016-10-26 11:38:36 +1300

    remove unused code from test

--- tcdebrepo-server/src/test/java/debrepo/teamcity/ebean/CustomerTest.java
+++ tcdebrepo-server/src/test/java/debrepo/teamcity/ebean/CustomerTest.java
@@ -1,19 +1,13 @@
 package debrepo.teamcity.ebean;
 
-import static org.junit.Assert.*;
-
-import org.avaje.datasource.DataSourceConfig;
-import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 import java.io.File;
 
-import com.avaje.ebean.EbeanServer;
-import com.avaje.ebean.EbeanServerFactory;
-import com.avaje.ebean.config.ServerConfig;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import debrepo.teamcity.ebean.server.EbeanServerProvider;
 import jetbrains.buildServer.serverSide.ServerPaths;
@@ -42,30 +36,4 @@ public class CustomerTest {
 		assertEquals(1,Customer.find.where().ilike("name", "netwolfuk").findCount());
 	}
 	
-	public EbeanServer getObject() throws Exception {
-
-		  ServerConfig config = new ServerConfig();
-		  config.setName("db");
-		  //config.setDatabase
-		  config.loadFromProperties();
-		  
-		  DataSourceConfig dsConfig = config.getDataSourceConfig();
-		  dsConfig.setUrl("jdbc:h2:file:~/h2-test-db;DB_CLOSE_ON_EXIT=FALSE");
-		  
-		  config.setDataSourceConfig(dsConfig);
-		  
-		  System.out.println(config.getDataSourceConfig().getUsername());
-		  
-		  // load test-ebean.properties if present for running tests
-		  // typically using H2 in memory database
-		  //config.loadTestProperties();
-
-		  // set as default and register so that Model can be
-		  // used if desired for save() and update() etc
-		  config.setDefaultServer(true);
-		  config.setRegister(true);
-
-		  return EbeanServerFactory.create(config);
-		}
-
 }
