d9ba9c717583626bada8bba31b96ff8a17c39836
Net Wolf UK <netwolfuk@gmail.com>
2016-11-10 18:56:08 +1300

    Ebean finding classes & writing to DB using TeamCity classloader.
    See: https://youtrack.jetbrains.com/issue/TW-47673
    and https://groups.google.com/forum/#!topic/ebean/8PBPHEAAHGc

--- tcdebrepo-server/src/main/java/debrepo/teamcity/ebean/server/EbeanServerProvider.java
+++ tcdebrepo-server/src/main/java/debrepo/teamcity/ebean/server/EbeanServerProvider.java
@@ -18,16 +18,100 @@ import com.avaje.ebean.config.ServerConfig;
 
 import debrepo.teamcity.Loggers;
 import jetbrains.buildServer.serverSide.ServerPaths;
+import jetbrains.buildServer.util.FuncThrow;
+import jetbrains.buildServer.util.Util;
+import jetbrains.buildServer.plugins.classLoaders.TeamCityClassLoader;
 
 public class EbeanServerProvider {
 
-	final EbeanServer myEbeanServer;
-
-	public EbeanServerProvider(ServerPaths serverPaths) {
+	EbeanServer myEbeanServer = null;
+	
+	/**
+	 * EbeanServerProvider constructor for unit tests that does not need TeamCity running.
+	 * 
+	 * @param serverPaths Used to find the directory to store the H2 files in.
+	 */
+	protected EbeanServerProvider(ServerPaths serverPaths) {
+		Loggers.SERVER.info("EbeanServerProvider :: Getting EBeanServer via testing method.");
 		this.myEbeanServer = createEbeanServerInstance(serverPaths.getPluginDataDirectory());
 	}
+	
+	/** 
+	 * EbeanServerProvider
+	 * @param serverPaths The directory to store the H2 files in.
+	 * @param teamCityClassLoader The classloader to use to create the instance.
+	 */
+	public EbeanServerProvider(ServerPaths serverPaths, TeamCityClassLoader teamCityClassLoader) {
+		Loggers.SERVER.info("EbeanServerProvider :: Getting EBeanServer via TeamCity classpath method.");
+		try {
+			this.myEbeanServer = Util.doUnderContextClassLoader(teamCityClassLoader,
+					new EbeanServerProviderInstantiationFunction(serverPaths.getPluginDataDirectory())
+					);
+		} catch (Exception e) {
+			Loggers.SERVER.error("EbeanServerProvider :: Could not create eBean instance!");
+			Loggers.SERVER.debug(e);
+		}
+	}
+	
+	public class EbeanServerProviderInstantiationFunction implements FuncThrow<EbeanServer, Exception> {
+		final File path;
+		
+		public EbeanServerProviderInstantiationFunction(File dataDir) {
+			this.path = dataDir;
+		}
+
+		@Override
+		public EbeanServer apply() throws Exception {
+			return createEbeanServerInstance(this.path);
+		}
+		
+	}
+	
 
 	public EbeanServer createEbeanServerInstance(File pluginDataDirectory) {
+
+		if (pluginDataDirectory.exists() && pluginDataDirectory.isDirectory() && pluginDataDirectory.canWrite()) {
+			File myDataDir = new File(pluginDataDirectory + File.separator + "tcDebRepository");
+			if (!myDataDir.exists()) {
+				if (myDataDir.mkdir()){
+					Loggers.SERVER.info("tcDebRepository EbeanServerProvider : Created directory " + myDataDir.getAbsolutePath());
+				} else {
+				    Loggers.SERVER.error("tcDebRepository EbeanServerProvider : Failed to create directory " + myDataDir.getAbsolutePath());
+				}
+			}
+
+			if (myDataDir.exists() && myDataDir.isDirectory() && myDataDir.canWrite()) {
+				ServerConfig config = new ServerConfig();
+				config.setName("db");
+				config.loadFromProperties();
+
+				DataSourceConfig dsConfig = config.getDataSourceConfig();
+				dsConfig.setUrl(
+						"jdbc:h2:file:" + myDataDir.getAbsolutePath() + File.separator + "tcDebRepositoryDB;DB_CLOSE_ON_EXIT=FALSE");
+
+				config.setDataSourceConfig(dsConfig);
+
+				Loggers.SERVER.debug(config.getDataSourceConfig().getUsername());
+
+				// load test-ebean.properties if present for running tests
+				// typically using H2 in memory database
+				// config.loadTestProperties();
+
+				// set as default and register so that Model can be
+				// used if desired for save() and update() etc
+				config.setDefaultServer(true);
+				config.setRegister(true);
+				config.addPackage("debrepo.teamcity.ebean");
+				
+				Loggers.SERVER.debug("EbeanServerProvider :: ebean Properties are: " + config.getProperties().toString());
+
+				return EbeanServerFactory.create(config);
+			}
+		}
+		return null;
+	}
+
+	public EbeanServer createEbeanServerInstance2(File pluginDataDirectory) {
 
 		if (pluginDataDirectory.exists() && pluginDataDirectory.isDirectory() && pluginDataDirectory.canWrite()) {
 			File myDataDir = new File(pluginDataDirectory + File.separator + "tcDebRepository");
@@ -47,15 +131,16 @@ public class EbeanServerProvider {
 						props = PropertiesLoaderUtils.loadAllProperties("ebean.properties", cl);
 					}
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					Loggers.SERVER.error("EbeanServerProvider :: Failed to load ebean.properties");
+					Loggers.SERVER.debug(e);
 				}
 				
-				Loggers.SERVER.debug("tcDebRepository:: ebean Properties are: " + props);
+				Loggers.SERVER.debug("tcDebRepository:: ebean Properties are: " + props.toString());
 				
 				ServerConfig config = new ServerConfig();
 				config.setName("db");
 				config.loadFromProperties();
+				config.addPackage("debrepo.teamcity.ebean");
 
 				DataSourceConfig dsConfig = config.getDataSourceConfig();
 				
--- tcdebrepo-server/src/test/java/debrepo/teamcity/ebean/server/CustomerTest.java
+++ tcdebrepo-server/src/test/java/debrepo/teamcity/ebean/server/CustomerTest.java
@@ -1,15 +1,18 @@
-package debrepo.teamcity.ebean;
+package debrepo.teamcity.ebean.server;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 import java.io.File;
+import java.net.URL;
 
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import debrepo.teamcity.ebean.Customer;
 import debrepo.teamcity.ebean.server.EbeanServerProvider;
+import jetbrains.buildServer.plugins.classLoaders.TeamCityClassLoader;
 import jetbrains.buildServer.serverSide.ServerPaths;
 
 public class CustomerTest {
@@ -29,7 +32,7 @@ public class CustomerTest {
 		//EbeanServer server = ebeanServerProvider.createEbeanServerInstance(pluginDataDirectory);
 		
 		Customer c = new Customer();
-		c.name = "netwolfuk";
+		c.setName("netwolfuk");
 		c.save();
 		
 		//fail("Not yet implemented");
